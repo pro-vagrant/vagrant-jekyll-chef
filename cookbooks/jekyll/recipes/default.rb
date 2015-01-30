fc30aa63028f345862955865b3fbe513a6f69277
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-01-30 09:50:43 +0100

    Fix gitignore and format

--- cookbooks/jekyll/recipes/default.rb
+++ cookbooks/jekyll/recipes/default.rb
@@ -4,5 +4,3 @@ package 'lynx-cur'
 execute 'gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3'
 execute 'curl -sSL https://get.rvm.io | bash -s stable --ruby'
 execute '/bin/bash --login -c "gem install jekyll"'
-
-
