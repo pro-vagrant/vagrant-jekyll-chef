d327e04792f775f0f93be2b2346fae838a9c5276
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-04-17 13:20:48 +0200

    Use ruby-1.9.1-dev instead of RVM

--- cookbooks/jekyll/recipes/default.rb
+++ cookbooks/jekyll/recipes/default.rb
@@ -1,6 +1,6 @@
 execute 'apt-get update'
 package 'nodejs'
 package 'lynx-cur'
-execute 'gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3'
-execute 'curl -sSL https://get.rvm.io | bash -s stable --ruby'
-execute '/bin/bash --login -c "gem install jekyll"'
+package 'ruby1.9.1-dev'
+execute 'gem install jekyll'
+
